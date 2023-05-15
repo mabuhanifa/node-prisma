802ff6eb91d85483c051e38cf59f116d697d844d
shourovahmd <moh.abuhanifa@gmail.com>
2023-05-15 16:03:48 +0600

    api running

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,20 @@
 import { PrismaClient } from "@prisma/client";
 import express from "express";
-
+const port = process.env.PORT || 3000;
 const app = express();
+app.use(express.json());
 const client = new PrismaClient();
 
-app.get("/", (req: any, res: any) => {
-  res.send("Hello World");
+app.post("/", (req: any, res: any) => {
+  const data = req.body;
+  try {
+    console.log(data);
+    res.send(data);
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+app.listen(port, () => {
+  console.log(`App listening on port ${port}`);
 });
