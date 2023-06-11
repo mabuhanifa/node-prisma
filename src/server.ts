57886f049fe94203f9b17cd3d9845263e418404c
shourovahmd <moh.abuhanifa@gmail.com>
2023-06-11 16:13:20 +0600

    migration created

--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,22 @@
 import { PrismaClient } from "@prisma/client";
 import express, { Request, Response } from "express";
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 
 const app = express();
 app.use(express.json());
-const client = new PrismaClient();
-console.log(`object`);
-app.post("/", (req: Request, res: Response) => {
-  const data = req.body;
+const prisma = new PrismaClient();
+
+app.post("/", async (req: Request, res: Response) => {
+  const { username, password } = req.body;
+  console.log(req.body);
   try {
-    console.log(data);
-    res.send(data);
+    const user = prisma.user.create({
+      data: {
+        username: username,
+        password: password,
+      },
+    });
+    res.send(user);
   } catch (error) {
     console.log(error);
   }
